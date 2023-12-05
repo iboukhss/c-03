21c1b5e0052cff2dcc254aaec92af0217c296f3c
iboukhss <151830328+iboukhss@users.noreply.github.com>
2023-12-06 00:25:02 +0100

    ex00 ok

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -1,28 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	while ((*s1 == *s2) && *s1)
 	{
-		printf("s1: %c\ns2: %c\n", *s1, *s2);
 		++s1;
 		++s2;
 	}
-	printf("\ns1: %d\ns2: %d\n", *s1, *s2);
-	
 	return (*s1 - *s2);
 }
+
+#include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
 	char	*hello = "";
-	char	*salut = " ";
+	char	*salut = "ll";
 	int	ret1;
 	int ret2;
 
 	ret1 = strcmp(hello, salut);
 	ret2 = ft_strcmp(hello, salut);
 
-	printf("\nret1: %d\nret2: %d\n", ret1, ret2);
+	printf("1: %d\n2: %d\n", ret1, ret2);
 }
